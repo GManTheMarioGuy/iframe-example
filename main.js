d254000e730200f0a582967f8b167343a0f5e267
uuphoria <155935644+uuphoria2@users.noreply.github.com>
2024-03-11 23:20:10 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+let div = document.createElement("div");
+
+
+div.innerHTML = `
+
+  <iframe src="https://sm64js.com"
+
+          width="1920"
+
+          height="1080"
+
+          frameborder="0"
+
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+
+          allowfullscreen>
+
+  </iframe>
+
+`;
+
+
+let body = document.querySelector("body");
+
+body.innerHTML = "";
+
+body.appendChild(div);
+
+
+// Set the iframe to fullscreen
+
+let iframe = div.querySelector("iframe");
+
+iframe.style.position = "fixed";
+
+iframe.style.top = "0";
+
+iframe.style.left = "0";
+
+iframe.style.width = "100%";
+
+iframe.style.height = "100%"; // Made by uuphoria github.com/uuphoria2
